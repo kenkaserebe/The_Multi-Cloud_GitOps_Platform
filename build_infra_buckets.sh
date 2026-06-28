7a310eb5978c867742d7ee15a0df50d851917bb0
kazaraal <araalkaz@gmail.com>
2026-06-28 03:17:54 +0100

    Edited The_Multi-Cloud_GitOps_Platform/build_infra.sh to include "terraform init"

--- build_infra_buckets.sh
+++ build_infra_buckets.sh
@@ -3,6 +3,14 @@
 #!/bin/bash
 
 # Create the AWS bucket to house the state file for AWS infrastructure
-MODULE_PATH="1-of-5_bootstrap/aws"
-#ls $MODULE_PATH
-terraform -chdir="$MODULE_PATH" apply --auto-approve
+AWS_MODULE_PATH="1-of-5_bootstrap/aws"
+
+terraform -chdir="$AWS_MODULE_PATH" init
+terraform -chdir="$AWS_MODULE_PATH" apply --auto-approve
+
+# Create the Azure bucket to house the state file for Azure infrastructure
+# Remember to run "az login" in shell to log into Azure before running the below
+AZURE_MODULE_PATH="1-of-5_bootstrap/azure"
+
+terraform -chdir="$AZURE_MODULE_PATH" init
+terraform -chdir="$AZURE_MODULE_PATH" apply --auto-approve
